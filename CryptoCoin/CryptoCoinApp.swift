a87db31894ed11c6078ff48033e8bf119f8ee76d
Prashuk <prashuk.ajmera@gmail.com>
2020-12-24 16:31:42 -0600

    Initial Commit

---
+++ CryptoCoin/CryptoCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCoinApp.swift
+//  CryptoCoin
+//
+//  Created by Prashuk Ajmera on 12/24/20.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
